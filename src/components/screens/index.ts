37c96f8b62a3aa017b9171aa30c87e070e19dd3f
oltree <o.melex@bk.ru>
2023-05-12 17:36:27 +0300

    fixed registers in imports

--- src/components/screens/index.ts
+++ src/components/screens/index.ts
@@ -1,8 +1,8 @@
 import Home from '@/components/screens/home/Home';
 import Notifications from '@/components/screens/notifications/Notifications';
 import Profile from '@/components/screens/profile/Profile';
-import SignIn from '@/components/screens/signin/SignIn';
-import SignUp from '@/components/screens/signup/SignUp';
+import SignIn from '@/components/screens/sign-in/SignIn';
+import SignUp from '@/components/screens/sign-up/SignUp';
 import Welcome from '@/components/screens/welcome/Welcome';
 import Workout from '@/components/screens/workout/Workout';
 
